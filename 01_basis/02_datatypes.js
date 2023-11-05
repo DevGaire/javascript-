e77d7344e63bc6d6c6adca0fa3bc746a7b148cd8
Devgaire <hendrygaire@gmail.com>
2023-11-05 11:58:48 +0545

    Datatypes

--- 01_basis/02_datatypes.js
+++ 01_basis/02_datatypes.js
@@ -1,1 +1,17 @@
 "use strict" ; 
+//alert(Dev) we are using nodejs
+
+let name = "dev";// strng
+let age = 19;// init 
+let isLoggIn = true; // boolean
+let state; // undefine
+
+//-------------->Datatype in Javascript<------------
+//string --> "" a to z or A to z or @#%^&*
+// number -->  2 to 2^560
+// boolean --> true/false // or on/off or 0/1
+// null -->  stanalone vlaue
+// undefined --> empty value
+
+console.log(name);
+console.log(typeof "dev")
