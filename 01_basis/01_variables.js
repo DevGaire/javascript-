b4062872da2e60212409208279523367e91f8fca
Devgaire <hendrygaire@gmail.com>
2023-11-05 11:33:47 +0545

    declaring variables

---
+++ 01_basis/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 123 // const value cannot be change or edit 
+let accountEmail = "test@gmail.com" /// let variable that cab be change or edit whenever you wants 
+let accountPassword = " 0001"
+var accountCity = "BUTWAL" 
+let accountState; // here we do not declare  value it show (undefined), In js it is possible to declare variable without asssign value
+
+
+accountEmail = "hg@gmail.com" // changing the value
+accountCity= "kathmandu"
+
+/* 
+we donot use var in the js  and this is multiple line comments.
+*/
+
+console.table([accountId, accountEmail, accountCity, accountPassword, accountState])
